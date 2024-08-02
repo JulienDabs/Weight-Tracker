1e2c050923adeb5346366aedba9d345bc5a9c44f
unknown <j.dabadie81@gmail.com>
2024-08-02 10:39:43 +0200

    setup React

---
+++ App/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+ 
+import "./App.css";
+
+function App() {
+   
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to Weight Tracker</h1>
+        <h5>App under construction</h5>
+      </div>
+    </>
+  );
+}
+
+export default App;
